1d0644d2439bc364a27e18554b463c9a1fd001ec
Andrew Quinn <53230903+hiAndrewQuinn@users.noreply.github.com>
2024-02-24 21:09:39 +0200

    feat: Heuristic based check for create-markdown-versions

--- create-markdown-versions.fish
+++ create-markdown-versions.fish
@@ -9,7 +9,9 @@ touch $hash
 
 for source_file in (find $source_dir -type f -name "*.html")
     if grep -q (sha1sum $source_file | awk '{print $2, $1}') $hash
+        set_color yellow
         echo "no changes in" $source_file ", skipping."
+        set_color normal
         continue # skip to the next iteration of the loop if the hash is found
     end
 
@@ -23,4 +25,17 @@ for source_file in (find $source_dir -type f -name "*.html")
     set dest_file "$dest_dir/_index.fi.md"
 
     html2md $source_file $dest_file
+
+    # Heuristic: Almost all of our articles have over 50 lines in them after
+    # processing with html2md. If we get a file with under 30 lines, something
+    # probably went wrong, and we should raise an error.
+    if test (wc -l $dest_file | awk '{print $1}') -lt 30
+        set_color red
+        echo "Error: $dest_file has less than 30 lines. Check it to see if something went wrong here."
+        exit 1
+    else
+        set_color green
+        echo "Success! $dest_file has over 30 lines."
+        set_color normal
+    end
 end
